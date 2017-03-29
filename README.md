2e25078347af91dc9dd2697abbdfddc0e22a9369
J-Rojas <jose@sano.co>
2017-03-29 09:08:07 -0700

    Updates to use message types instead of topic for message identification. Display multiple camera images by topic in different windows

--- README.md
+++ README.md
@@ -17,10 +17,20 @@ Most of these can be installed with a simple 'pip install MODULE' command
 
 * numpy
 * opencv (pip install opencv-python)
-* pygame
+* pyglet
 * rosbag
 * rospkg
 * cv_bridge (apt-get install ros-kinetic-cv-bridge)
+
+## KITTI to ROSbag conversion
+
+Install Kitti2Bag
+
+'pip install kitti2bag'
+
+To convert, follow the directions on the Github page. Pay attention to the how the zip files are unpacked.
+
+https://github.com/tomas789/kitti2bag
 
 ## Command Line
 
@@ -28,15 +38,27 @@ Basic command:
 
 `python view_rosbag_video.py --dataset <BAGFILE>`
 
+For visualizing only certain topics
+
+`python view_rosbag_video.py --dataset <BAGFILE> --topics <TOPIC1>,<TOPIC2>,...`
+
 To run via Docker on Mac:
 
-* Make sure you have XQuartz running
+* Install XQuartz running
+* Make sure you have updated XQuartz settings for GLX
+
+`defaults write org.macosforge.xquartz.X11 enable_iglx -bool true`
+
+* Log Out and Log back Into Mac session
 * Install socat on your Mac (brew install socat)
+
+Then these steps once per Mac session:
+
 * Run socat on your Mac host
 
 `socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\"`
 
 * Find out your host machine's IP address using 'ifconfig'
-* Setup your DISPLAY environment to point to your host's XQuartz
+* Within your Docker container, setup your DISPLAY environment to point to your host's XQuartz
 
 `export DISPLAY=<HOST_IP_ADDRESS>:0`    
--- view_rosbag_video.py
+++ view_rosbag_video.py
@@ -14,10 +14,10 @@ History:
 2016/10/02: Initial version to display left, center, right cameras and steering angle.
 """
 
+import pyglet
 import argparse
 import sys
 import numpy as np
-import pygame
 import rosbag
 import datetime
 import sensor_msgs.point_cloud2
@@ -27,102 +27,21 @@ from cv_bridge import CvBridge
 
 appTitle = "Udacity Team-SF: ROSbag viewer"
 bridge = CvBridge()
-pygame.init()
 video_max_width=875
 size = None
 startsec = 0
-pygame.display.set_caption(appTitle)
-screen = None
+windowCamera = {}
+windowLidar = None
+windowRadar = None
 
-# ***** get perspective transform for images *****
-from skimage import transform as tf
-
-rsrc = \
- [[43.45456230828867, 118.00743250075844],
-  [104.5055617352614, 69.46865203761757],
-  [114.86050156739812, 60.83953551083698],
-  [129.74572757609468, 50.48459567870026],
-  [132.98164627363735, 46.38576532847949],
-  [301.0336906326895, 98.16046448916306],
-  [238.25686790036065, 62.56535881619311],
-  [227.2547443287154, 56.30924933427718],
-  [209.13359962247614, 46.817221154818526],
-  [203.9561297064078, 43.5813024572758]]
-rdst = \
- [[10.822125594094452, 1.42189132706374],
-  [21.177065426231174, 1.5297552836484982],
-  [25.275895776451954, 1.42189132706374],
-  [36.062291434927694, 1.6376192402332563],
-  [40.376849698318004, 1.42189132706374],
-  [11.900765159942026, -2.1376192402332563],
-  [22.25570499207874, -2.1376192402332563],
-  [26.785991168638553, -2.029755283648498],
-  [37.033067044190524, -2.029755283648498],
-  [41.67121717733509, -2.029755283648498]]
-
-tform3_img = tf.ProjectiveTransform()
-tform3_img.estimate(np.array(rdst), np.array(rsrc))
-
-def perspective_tform(x, y):
-  p1, p2 = tform3_img((x,y))[0]
-  return p2, p1
-
-# ***** functions to draw lines *****
-def draw_pt(img, x, y, color, shift_from_mid, sz=1):
-  col, row = perspective_tform(x, y)
-  row = int(row) + shift_from_mid
-  col = int(col+img.get_height()*2)/3
-  if row >= 0 and row < img.get_width()-sz and\
-     col >= 0 and col < img.get_height()-sz:
-    img.set_at((row-sz,col-sz), color)
-    img.set_at((row-sz,col), color)
-    img.set_at((row-sz,col+sz), color)
-    img.set_at((row,col-sz), color)
-    img.set_at((row,col), color)
-    img.set_at((row,col+sz), color)
-    img.set_at((row+sz,col-sz), color)
-    img.set_at((row+sz,col), color)
-    img.set_at((row+sz,col+sz), color)
-
-def draw_path(img, path_x, path_y, color, shift_from_mid):
-  for x, y in zip(path_x, path_y):
-    draw_pt(img, x, y, color, shift_from_mid)
-
-# ***** functions to draw predicted path *****
-
-def calc_curvature(v_ego, angle_steers, angle_offset=0):
-  deg_to_rad = np.pi/180.
-  slip_fator = 0.0014 # slip factor obtained from real data
-  steer_ratio = 15.3  # from http://www.edmunds.com/acura/ilx/2016/road-test-specs/
-  wheel_base = 2.67   # from http://www.edmunds.com/acura/ilx/2016/sedan/features-specs/
-
-  angle_steers_rad = (angle_steers - angle_offset) * deg_to_rad
-  curvature = angle_steers_rad/(steer_ratio * wheel_base * (1. + slip_fator * v_ego**2))
-  return curvature
-
-def calc_lookahead_offset(v_ego, angle_steers, d_lookahead, angle_offset=0):
-  #*** this function returns the lateral offset given the steering angle, speed and the lookahead distance
-  curvature = calc_curvature(v_ego, angle_steers, angle_offset)
-
-  # clip is to avoid arcsin NaNs due to too sharp turns
-  y_actual = d_lookahead * np.tan(np.arcsin(np.clip(d_lookahead * curvature, -0.999, 0.999))/2.)
-  return y_actual, curvature
-
-def draw_path_on(img, speed_ms, angle_steers, color=(0,0,255), shift_from_mid=0):
-  path_x = np.arange(0., 50.1, 0.5)
-  path_y, _ = calc_lookahead_offset(speed_ms, angle_steers, path_x)
-  draw_path(img, path_x, path_y, color, shift_from_mid)
-
-def print_msg(topic, msg, time):
+def print_msg(msgType, topic, msg, time):
 
     t = time.to_sec()
-    since_start = msg.header.stamp.to_sec()-startsec
 
-    if topic in ['/rosout','/cloud_nodelet/parameter_descriptions','/tf', '/cloud_nodelet/parameter_updates']:
-        pass
-    elif topic in ['/radar/tracks','/radar/range','/obs1/gps/time','/gps/time']:
-        pass
-    elif topic in ['/radar/points','/velodyne_points']:
+    if 'sensor_msgs' in msgType or 'nav_msgs' in msgType:
+        since_start = msg.header.stamp.to_sec()-startsec
+
+    if msgType in ['sensor_msgs/PointCloud2']:
 
         points = sensor_msgs.point_cloud2.read_points(msg, skip_nans=True)
         arrPoints = []
@@ -133,19 +52,21 @@ def print_msg(topic, msg, time):
             arrPoints.append(point)
         print(topic, msg.header.seq, since_start, 'nPoints=', len(arrPoints), t)
 
-    elif topic in ['/gps/rtkfix','/obs1/gps/rtkfix']:
+    elif msgType in ['sensor_msgs/NavSatFix']:
+        print(topic, msg.header.seq, since_start, msg.latitude, msg.longitude, msg.altitude, t)
 
-        #print(topic, msg.header.seq, t-tsec, 'nPoints=', len(point))
-        pass
+    elif msgType in ['nav_msgs/Odometry']:
+        position = msg.pose.pose.position
+        print(topic, msg.header.seq, since_start, position.x, position.y, position.z, t)
 
     elif topic == '/radar/range':
 
         print(topic, msg.header.seq, since_start, msg.radiation_type, msg.field_of_view, msg.min_range, msg.max_range, msg.range, t)
 
-    elif topic == '/image_raw':
-        print(topic, msg.header.seq, since_start, t)
-    elif topic == '/gps/fix':
-        print(topic, msg.header.seq, since_start, msg.latitude, msg.longitude, msg.altitude, t)
+    elif msgType == 'sensor_msgs/Image':
+        print(topic, msg.header.seq, msg.width, msg.height, since_start, t)
+    elif msgType in ['sensor_msgs/CameraInfo']:
+        print(topic, msg.header.seq, since_start, msg.width, msg.height, msg.distortion_model, t)
     else:
         pass
         #print(topic, msg.header.seq, t-msg.header.stamp, msg, t)
@@ -155,22 +76,20 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser(description=appTitle)
   parser.add_argument('--dataset', type=str, default="dataset.bag", help='Dataset/ROS Bag name')
   parser.add_argument('--skip', type=int, default="0", help='skip seconds')
+  parser.add_argument('--topics', type=str, default=None, help='Topic list to display')
   args = parser.parse_args()
 
   dataset = args.dataset
   skip = args.skip
   startsec = 0
-  topics_list = [
-    '/image_raw',
-    '/gps/fix',
-    '/radar/points',
-    '/velodyne_points',
-    '/radar/range'
-  ]
+  topics_list = args.topics.split(',') if args.topics else None
 
   print("reading rosbag ", dataset)
   bag = rosbag.Bag(dataset, 'r')
+  topicTypesMap = bag.get_type_and_topic_info().topics
+
   for topic, msg, t in bag.read_messages(topics=topics_list):
+    msgType = topicTypesMap[topic].msg_type
     if startsec == 0:
         startsec = t.to_sec()
         if skip < 24*60*60:
@@ -180,26 +99,44 @@ if __name__ == "__main__":
             skipping = skip
             print("skipping to ", skip, " from ", startsec, " ...")
     else:
+        window = None
         if t.to_sec() > skipping:
-            print_msg(topic, msg, t)
+            print_msg(msgType, topic, msg, t)
 
-            if topic in ['/image_raw']:
-                #RGB_str = msg.data
+            if msgType in ['sensor_msgs/Image']:
+
                 cv_img = bridge.imgmsg_to_cv2(msg, desired_encoding="rgb8")
-                cv_img = cv_img.transpose((1, 0, 2))
+                cv_img = np.flipud(cv_img)
 
                 if (size == None):
-                    ratio = float(cv_img.shape[1]) / video_max_width
-                    size = (int(ratio * cv_img.shape[0]), int(ratio * cv_img.shape[1]))
-                    screen = pygame.display.set_mode(size, pygame.DOUBLEBUF)
+                    ratio = float(cv_img.shape[0]) / video_max_width
+                    size = (int(ratio * cv_img.shape[1]), int(ratio * cv_img.shape[0]))
+                    print(size)
+                if (windowCamera.get(topic, None) == None):
+                    windowCamera[topic] = pyglet.window.Window(size[0], size[1], caption=topic)
 
-                img = pygame.pixelcopy.make_surface(cv_img)
-                img = pygame.transform.scale(img, size)
+                window = windowCamera[topic]
+                img = pyglet.image.ImageData(cv_img.shape[1], cv_img.shape[0], 'RGB', cv_img.tobytes())
+
+            #elif topic in ['sensor_msgs/PointCloud2']:
+
+                # render top down point cloud
+
+            #    if (size == None):
+            #        ratio = float(cv_img.shape[0]) / video_max_width
+            #        size = (int(ratio * cv_img.shape[1]), int(ratio * cv_img.shape[0]))
+            #        print(size)
+            #        windowCamera = pyglet.window.Window(size[0], size[1])
+
+            #    window = windowCamera
+            #    img = pyglet.image.ImageData(cv_img.shape[1], cv_img.shape[0], 'RGB', cv_img.tobytes())
 
             #draw_path_on(imgleft, 0, angle_steers*20, (0,0,255), -10)
             #draw_path_on(imgcenter, 0, angle_steers*20, (0,0,255), -20)
             #draw_path_on(imgright, 0, angle_steers*20, (0,0,255), 0)
 
-            if screen:
-                screen.blit(img, (0,0))
-                pygame.display.flip()
+            if window:
+                window.switch_to()
+                window.dispatch_events()
+                img.blit(0,0,width=size[0], height=size[1])
+                window.flip()
